1d6f3033b56805ab7285094c63e5ca40d12481b4
unknown <eliasnegasa5@gmail.com>
2020-11-10 04:16:38 +0300

    final push update pdf

--- Clinic_Management_System/settings.py
+++ Clinic_Management_System/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'v2n@u*!9+*991$^y89e-o(9b47mr2mxmj=smsye^t20&m1fvy!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
